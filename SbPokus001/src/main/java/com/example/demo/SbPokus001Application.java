4f511ca9c0fe7ea5b519d976ef49d516e497f0ab
jaslama <46301529+jaslama@users.noreply.github.com>
2019-07-28 22:29:14 +0200

    Add files via upload
    commituju :)

--- SbPokus001/src/main/java/com/example/demo/SbPokus001Application.java
+++ SbPokus001/src/main/java/com/example/demo/SbPokus001Application.java
@@ -10,7 +10,9 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SbPokus001Application {
 
 	public static void main(String[] args) {
+		System.out.println("START start");
 		SpringApplication.run(SbPokus001Application.class, args);
+		System.out.println("START end");
 	}
 
 }
